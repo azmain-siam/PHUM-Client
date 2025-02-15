6e99578eed487f2e797e2296c0561dcdecee6fc3
azmain-siam <siamhossain2700@gmail.com>
2025-02-15 15:56:24 +0600

    fix: routes

---
+++ src/components/layout/Sidebar.tsx
@@ -0,0 +1,71 @@
+import { Layout, Menu } from "antd";
+import { sidebarItemsGenerator } from "../../utils/sidebarItemsGenerator";
+import { adminPaths } from "../../routes/admin.routes";
+import { FacultyPaths } from "../../routes/faculty.routes";
+import { studentPaths } from "../../routes/student.routes";
+const { Sider } = Layout;
+
+const userRole = {
+  ADMIN: "admin",
+  FACULTY: "faculty",
+  STUDENT: "student",
+};
+
+const Sidebar = () => {
+  const role = "admin";
+
+  let sidebarItems;
+
+  switch (role) {
+    case userRole.ADMIN:
+      sidebarItems = sidebarItemsGenerator(adminPaths, userRole.ADMIN);
+      break;
+
+    case userRole.FACULTY:
+      sidebarItems = sidebarItemsGenerator(FacultyPaths, userRole.FACULTY);
+      break;
+
+    case userRole.STUDENT:
+      sidebarItems = sidebarItemsGenerator(studentPaths, userRole.STUDENT);
+      break;
+
+    default:
+      break;
+  }
+
+  return (
+    <Sider
+      breakpoint="lg"
+      collapsedWidth="0"
+      // onBreakpoint={(broken) => {
+      //   console.log(broken);
+      // }}
+      // onCollapse={(collapsed, type) => {
+      //   console.log(collapsed, type);
+      // }}
+    >
+      <div
+        style={{
+          color: "#fff",
+          textAlign: "center",
+          fontWeight: "bold",
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          height: "4rem",
+          fontSize: "22px",
+        }}
+      >
+        PHUM
+      </div>
+      <Menu
+        theme="dark"
+        mode="inline"
+        defaultSelectedKeys={["4"]}
+        items={sidebarItems}
+      />
+    </Sider>
+  );
+};
+
+export default Sidebar;
